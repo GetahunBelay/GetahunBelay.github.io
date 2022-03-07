0daf9dd9737fe6619d2aa4f5ab7a642a54057e21
GetahunBelay <getishe@gmail.com>
2022-03-06 23:36:22 -0600

    Update arrayPractice.js

--- d25arrayPractice/arrayPractice.js
+++ d25arrayPractice/arrayPractice.js
@@ -1,55 +1,52 @@
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
 //module.exports = { doubleNums,doubleAges, filterEven, filterOver10, findEvenNum, findEvenAge, includesEvenNum}; //add all of your function names here that you need for the node mocha tests
 
-let numArray;
-let peopleArray;
+// let numArray;
+// let peopleArray;
 
 
-    function doubleNums(arr){
+function doubleNums(arr){
 
     return arr.map(num => num * 2);
-    }
-    
-    
+   
+}
+       
 
-    function doubleAges(arr){
+function doubleAges(arr){
 
-    return arr.map(item => doubl(item));
+    return arr.map(item => item.age * 2);
 
 }
       
-       
+          
+function filterEven(arr){
 
+    return arr.filter(num => num % 2 === 0);
     
+}
     
-    function filterEven(arr){
+function filterOver10(arr) {
 
-        return arr.filter(num => num % 2 === 0);
+    return arr.filter(num => (num.age) > 10); 
     
-    }
+}
     
-    function filterOver10(arr) {
+function findEvenNum(arr){
 
-       return arr.filter(num => (num.age) > 10); 
+    return arr.find(num => num % 2 === 0)
     
-    }
+}
     
-    function findEvenNum(arr){
+function findEvenAge(arr){
 
-        return arr.find(num => num % 2 === 0)
+    return arr.find(num => (num.age) % 2 === 0);
     
-    }
+}
     
-    function findEvenAge(arr){
+function includesEvenNum(arr){
 
-        return arr.find(num => (num.age) % 2 === 0);
+    return arr.includes(num => arr.includes(num % 2 === 0))
     
-    }
-    
-    function includesEvenNum(arr){
-
-        return arr.includes(num => arr.includes(num % 2 === 0))
-    
-    }
+}
     
     
